7a41169b393edc0e63454b323ec1523f15b91617
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-06-22 15:17:16 +0200

    enabled filter by platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,24 +1,50 @@
 import {
-  IconButton,
+  Button,
+  Flex,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  Text,
 } from "@chakra-ui/react";
 import { FaChevronDown } from "react-icons/fa";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectedPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
   const { data } = usePlatforms();
   return (
     <Menu>
-      <MenuButton display="flex">
-        Platform
-        <IconButton icon={<FaChevronDown />} aria-label={"chevron down"} />
+      <MenuButton
+        as={Button}
+        rightIcon={<FaChevronDown />}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        minWidth="200px"
+        bg="gray.700"
+        color="white"
+        _hover={{ bg: "gray.600" }}
+        _active={{ bg: "gray.800" }}
+      >
+        <Flex align="center">
+          <Text mr={2}>{selectedPlatform?.name || "Select Platform"}</Text>
+        </Flex>
       </MenuButton>
-      <MenuList>
+      <MenuList bg="gray.700" color="white">
         {data?.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectedPlatform(platform)}
+            _hover={{ bg: "gray.600" }}
+            _focus={{ bg: "gray.600" }}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
