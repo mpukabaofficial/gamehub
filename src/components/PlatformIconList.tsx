0cd570dc2c2973adfd0e4f4eb364b1794aa76d12
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-06-22 20:08:38 +0200

    Build search input

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,7 +32,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.name]} color="gray.500" />
+        <Icon key={platform.id} as={iconMap[platform.name]} color="gray.500" />
       ))}
     </HStack>
   );
